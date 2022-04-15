40a83947f30ade627c620d9ab33b5b22cde8e376
Andrew Weller <andrew.weller@colorado.edu>
2022-04-15 12:58:25 -0600

    Change Directory Structures

--- web/sites/default/files/asset_injector/js/booklist_height-7cbc7cf20da6528a4791b7880f3174f3.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('mousemove', (mouseEvent) => {
-	console.log(`Mouse X: ${mouseEvent.screenY}, Mouse Y: ${mouseEvent.screenY}`);
-	document.documentElement.style.setProperty(`--height`, `${mouseEvent.screenY+"px"}`);
-});
