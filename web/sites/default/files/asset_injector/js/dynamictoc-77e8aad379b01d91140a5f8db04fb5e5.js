0a7f959e67e2b9ead644f49a7b15fb8c0e73a1fd
Robot <none@none.com>
2022-04-20 07:05:32 +0000

    Putting simple directory changes on master

--- web/sites/default/files/asset_injector/js/dynamictoc-77e8aad379b01d91140a5f8db04fb5e5.js
+++
@@ -1,9 +0,0 @@
-/*Note: The number in item needs to change if there are more than one view in the page, depending on which view-content it is grabbing from, the code may break*/
-console.log("Applying JS to TOC... ");
-// var yPos = document.querySelector('.view-content').offsetTop;
-// var yPos = document.documentElement.querySelectorAll('.view-content').item(1).offsetTop;
-var arr = document.documentElement.querySelectorAll('.view-content')
-var yPos = arr[arr.length - 1].offsetTop;
-yPos = yPos - 10;
-document.documentElement.style.setProperty(`--height`, `${yPos+"px"}`);
-
