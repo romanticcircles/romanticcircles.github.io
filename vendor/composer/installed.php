d6739b9643809d3ee0ccfa488357e5a6bc50b60b
tj mclemore <tjmclemore@gmail.com>
2022-04-28 16:10:04 -0600

    new module test (webroot)

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '619c8d2601ce900639dfbde9948f8b169cb62bf1',
+        'reference' => '048a39233b8db5f10e4b38a8b016230508d713ed',
         'name' => 'drupal/recommended-project',
         'dev' => true,
     ),
@@ -820,7 +820,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '619c8d2601ce900639dfbde9948f8b169cb62bf1',
+            'reference' => '048a39233b8db5f10e4b38a8b016230508d713ed',
             'dev_requirement' => false,
         ),
         'drupal/responsive_image' => array(
@@ -1008,6 +1008,15 @@
             'replaced' => array(
                 0 => '9.3.12',
             ),
+        ),
+        'drupal/webform' => array(
+            'pretty_version' => '6.1.3',
+            'version' => '6.1.3.0',
+            'type' => 'drupal-module',
+            'install_path' => __DIR__ . '/../../web/modules/contrib/webform',
+            'aliases' => array(),
+            'reference' => '6.1.3',
+            'dev_requirement' => false,
         ),
         'drupal/workflows' => array(
             'dev_requirement' => false,
