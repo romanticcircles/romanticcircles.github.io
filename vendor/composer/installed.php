ec4a9501476aadf28ffb144f0a352e9032b71b5b
tj mclemore <tjmclemore@gmail.com>
2022-04-28 16:37:22 -0600

    remove webroot module

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'cdce217879bdfdcb0be24d9e04fcf4e570440ef2',
+        'reference' => '1bfc0af457029b5fdfe45234ff8a599a7392186a',
         'name' => 'drupal/recommended-project',
         'dev' => true,
     ),
@@ -820,7 +820,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'cdce217879bdfdcb0be24d9e04fcf4e570440ef2',
+            'reference' => '1bfc0af457029b5fdfe45234ff8a599a7392186a',
             'dev_requirement' => false,
         ),
         'drupal/responsive_image' => array(
@@ -1008,15 +1008,6 @@
             'replaced' => array(
                 0 => '9.3.12',
             ),
-        ),
-        'drupal/webform' => array(
-            'pretty_version' => '6.1.3',
-            'version' => '6.1.3.0',
-            'type' => 'drupal-module',
-            'install_path' => __DIR__ . '/../../web/modules/contrib/webform',
-            'aliases' => array(),
-            'reference' => '6.1.3',
-            'dev_requirement' => false,
         ),
         'drupal/workflows' => array(
             'dev_requirement' => false,
